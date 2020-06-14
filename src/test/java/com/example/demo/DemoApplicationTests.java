aba3709ec16d835dcc0697926be1ad20aff281a3
pokev25 <pokev25@gmail.com>
2020-06-14 13:46:58 +0900

    mockmvc 테스트 추가 - 한글 깨지는 문제는 SpringConfig 추가 하여 해결 http://honeymon.io/tech/2019/10/23/spring-deprecated-media-type.html

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,21 +1,34 @@
 package com.example.demo;
 
-import com.example.demo.controller.UserProfileController;
 import com.example.demo.model.UserProfile;
 import com.example.demo.service.UserProfileService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.List;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @Slf4j
 @SpringBootTest
+@AutoConfigureMockMvc
 class DemoApplicationTests {
 
 	@Autowired
 	UserProfileService service;
+	@Autowired
+	MockMvc mockMvc;
+	@Autowired
+	ObjectMapper objectMapper;
 
 	@Test
 	void contextLoads() {
@@ -31,5 +44,15 @@ class DemoApplicationTests {
 	void getUserByID(){
 		UserProfile userProfile = service.getUserProfile(1);
 		log.info("user list : {}", userProfile);
+	}
+
+	@Test
+	void validTest() throws Exception {
+		ResultActions result = mockMvc.perform(get("/user/all")
+					.contentType(MediaType.APPLICATION_JSON));
+
+		result
+				.andDo(print())
+				.andExpect(status().isOk());
 	}
 }
