1e9cc46f7e1f98e972615d4e8f8914e6b9961563
pokev25 <pokev25@gmail.com>
2020-07-21 18:11:09 +0900

    test value 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,6 @@
 insert into UserProfile (id, name, phone, address) VALUES (1,'홍길동', '111-1111', '종로구 사직동');
 insert into UserProfile (id, name, phone, address) VALUES (2,'홍길순', '222-2222', '강남구 대치동');
+
+insert into member (name) VALUES ('spring1');
+insert into member (name) VALUES ('spring2');
+
